106eadb5c13d06950da5bb05fb2da18e4c4a0abc
hzd1188 <599030182@qq.com>
2017-09-12 14:26:38 +0800

    学科列表

--- src/js/category/edit.js
+++ src/js/category/edit.js
@@ -1,2 +1,33 @@
 require('../common/aside');
 require("../common/header")
+    // 引入暴露后的模块
+var util = require('../common/util');
+
+
+
+// 数据回显
+// 1，模块点方法过去模块方法中的返回值
+// 2，先获取location.search中的cg_id
+// 3,得到数据渲染后的模板，引入到页面中
+
+var cg_id = util.getSearch('cg_id');
+$.get('/v6/category/edit', { cg_id: cg_id }, function(data) {
+    var html = template('form-tpl', data.result);
+    $('.category-edit').html(html);
+
+})
+
+
+
+// 修改表单并提交
+
+// 因为数据要回显，所以form表单是异步动态插入到页面中的，使用ajaxForm插件时需要用到空控件
+
+$('#form-category').ajaxForm({
+    delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            alert('修改成功');
+        }
+    }
+})
--- src/js/common/b.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    aa: 111,
-    bb: 222,
-    cc: 333,
-    dd: 444,
-    ee: 555
-}
